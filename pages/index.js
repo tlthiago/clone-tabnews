3818e18bfe036f9e196dec317fd231fe001ac816
Thiago Alves <70245552+tlthiago@users.noreply.github.com>
2024-12-28 04:35:19 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Paulinho, eu amo você. Se você me ama, dá uma risadinha. 😍</h1>
+  return <h1>Paulinho, eu amo você. Se você me ama, dá uma risadinha. 😍</h1>;
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
