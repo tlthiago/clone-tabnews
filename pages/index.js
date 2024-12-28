fda74efa9509deef538a91c79e178d742f32792b
Thiago Alves <70245552+tlthiago@users.noreply.github.com>
2024-12-28 04:43:09 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,4 @@ function Home() {
   return <h1>Paulinho, eu amo você. Se você me ama, dá uma risadinha. 😍</h1>;
 }
 
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
