8db211e39c664446f0e399716a6624f446176a03
Thiago <thiagoluizmachadoalves@gmail.com>
2025-02-26 15:46:39 -0300

    feat: hide `migrator` logs

--- models/migrator.js
+++ models/migrator.js
@@ -1,12 +1,12 @@
 import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
-import database from "infra/database";
+import database from "infra/database.js";
 
 const defaultMigrationOptions = {
   dryRun: true,
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
