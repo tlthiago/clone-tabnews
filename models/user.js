7a675e91bb71d6924476abc8569a81ab73059f62
tlthiago <thiagoluizmachadoalves@gmail.com>
2025-03-21 21:42:07 -0300

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,36 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors";
+import { ValidationError, NotFoundError } from "infra/errors";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+        SELECT 
+          * 
+        FROM 
+          users  
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT
+          1
+        ;`,
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validadeUniqueEmail(userInputValues.email);
@@ -13,7 +44,8 @@ async function create(userInputValues) {
       text: `
         SELECT 
           email 
-        FROM users  
+        FROM 
+          users  
         WHERE
           LOWER(email) = LOWER($1)
         ;`,
@@ -33,7 +65,8 @@ async function create(userInputValues) {
       text: `
         SELECT 
           username 
-        FROM users  
+        FROM 
+          users  
         WHERE
           LOWER(username) = LOWER($1)
         ;`,
@@ -70,6 +103,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
