9ac517a556241d6810ef9acf8d223e5aa93758bd
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2025-04-09 15:29:17 -0300

    refactor: updates migration `users`

--- infra/migrations/1740592228503_create-users.js
+++ infra/migrations/1740592228503_create-users.js
@@ -17,19 +17,21 @@ exports.up = (pgm) => {
       notNull: true,
       unique: true,
     },
-    // Why 72 in length? https://security.stackexchange.com/a/39851
+    // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
     password: {
-      type: "varchar(72)",
+      type: "varchar(60)",
       notNull: true,
     },
     // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
     created_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
     updated_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
   });
 };
