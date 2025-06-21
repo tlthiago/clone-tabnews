4babe62c1c3e0013797ab202a80e10ec96a36d33
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2025-06-21 17:49:33 -0300

    feat: hash `password` in `user.create`

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import database from "infra/database.js";
+import password from "models/password.js";
 import { ValidationError, NotFoundError } from "infra/errors";
 
 async function findOneByUsername(username) {
@@ -35,6 +36,7 @@ async function findOneByUsername(username) {
 async function create(userInputValues) {
   await validadeUniqueEmail(userInputValues.email);
   await validadeUniqueUsername(userInputValues.username);
+  await hashPasswordInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
@@ -81,11 +83,16 @@ async function create(userInputValues) {
     }
   }
 
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+    userInputValues.password = hashedPassword;
+  }
+
   async function runInsertQuery(userInputValues) {
     const results = await database.query({
       text: `
         INSERT INTO 
-          users (username, email, password) 
+          users (username, email, password)  
         VALUES 
           ($1, $2, $3)
         RETURNING
