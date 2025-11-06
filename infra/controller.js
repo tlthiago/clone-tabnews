e0b9fd12fe8f3047478a481e15221ffe08b0b923
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2025-11-06 15:24:41 -0300

    feat: require `read:session` to access `/user` endpoint

--- infra/controller.js
+++ infra/controller.js
@@ -96,7 +96,7 @@ function canRequest(feature) {
     if (authorization.can(userTryingToRequest, feature)) return next();
     throw new ForbiddenError({
       message: "Você não possui permissão para executar essa ação.",
-      action: `Verifique se o seu usuário possui a feature ${feature}`,
+      action: `Verifique se o seu usuário possui a feature "${feature}"`,
     });
   };
 }
--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -1,23 +1,20 @@
 import { createRouter } from "next-connect";
-import controller from "infra/controller";
+import controller from "infra/controller.js";
 import user from "models/user.js";
-import session from "models/session";
+import session from "models/session.js";
 
 const router = createRouter();
-
-router.get(getHandler);
+router.use(controller.injectAnonymousOrUser);
+router.get(controller.canRequest("read:session"), getHandler);
 
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
   const sessionToken = request.cookies.session_id;
-
   const sessionObject = await session.findOneValidByToken(sessionToken);
   const renewedSessionObject = await session.renew(sessionObject.id);
   controller.setSessionCookie(renewedSessionObject.token, response);
-
   const userFound = await user.findOneById(sessionObject.user_id);
-
   response.setHeader(
     "Cache-Control",
     "no-store, no-cache, max-age=0, must-revalidade",
