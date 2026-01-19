809a307320c5784978a334ee09413b71e8b676c6
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2026-01-19 13:59:33 -0300

    feat: allow `update:user:others` to update other users

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -100,6 +100,11 @@ async function activateUser(inactiveUser) {
   return await activation.activateUserByUserId(inactiveUser.id);
 }
 
+async function addFeaturesToUser(userObject, features) {
+  const updatedUser = await user.addFeatures(userObject.id, features);
+  return updatedUser;
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
@@ -110,6 +115,7 @@ const orchestrator = {
   getLastEmail,
   extractUUID,
   activateUser,
+  addFeaturesToUser,
 };
 
 export default orchestrator;
